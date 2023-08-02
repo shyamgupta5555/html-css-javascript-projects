ce680453ace7019bd428bd46569a2dc1ceb504eb
shyam <shyamgupta0214@gmail.com>
2023-08-02 22:39:08 +0530

    added

---
+++ digital time/index.js
@@ -0,0 +1,32 @@
+const hour = document.getElementById("hour")
+const min = document.getElementById("minutes")
+const sec = document.getElementById("seconds")
+
+const ampmEl = document.getElementById("ampm")
+
+function updateClock(){
+  let h = new Date().getHours()
+  let m = new Date().getMinutes()
+  let s = new Date().getSeconds()
+  let ampm = "AM"
+
+  if(h >12){
+    h = h-12
+    ampm = "PM"
+  }
+
+  h = h <10 ? "0" +h :h;
+  m = m <10 ? "0" +m :m;
+  s = s <10 ? "0" +s:s;
+
+
+  hour.innerText=h
+  min.innerText = m
+  sec.innerText=s
+  ampmEl ,(innerText = ampm)
+  setTimeout(()=>{
+  updateClock()
+  },1000)
+}
+
+updateClock()
